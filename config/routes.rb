ac8c9cd8b108d58f6db04bbbf4ccd2c2e4b6551f
johnpeters <john.peters1815@gmail.com>
2015-12-31 14:20:54 +0000

    Styling improvements, added contact page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/about' => 'pages#about'
+  get '/contact' => 'pages#about'
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
